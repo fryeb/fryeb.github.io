0a01dbe72ac68dc60bc2324e86ff53def9862e6e
Benjamin Frye <ben.j.frye@gmail.com>
2019-09-03 11:40:54 +1000

    Display training images

--- _posts/2019-08-27-unrenderer-2d.md
+++ _posts/2019-08-27-unrenderer-2d.md
@@ -6,10 +6,10 @@ title: "Unrenderer"
 <canvas id="demo01_canvas" width="512px" height="512px">
 </canvas>
 <div class="unrender_image_container">
-	<img src="/assets/img/unrender01.jpg" alt="test01"/>
-	<img src="/assets/img/unrender02.jpg" alt="test02"/>
-	<img src="/assets/img/unrender03.jpg" alt="test03"/>
-	<img src="/assets/img/unrender04.jpg" alt="test04"/>
+	<img src="/assets/img/unrender01.jpg" alt="test01" onclick="textureIndex = 0"/>
+	<img src="/assets/img/unrender02.jpg" alt="test01" onclick="textureIndex = 1"/>
+	<img src="/assets/img/unrender03.jpg" alt="test01" onclick="textureIndex = 2"/>
+	<img src="/assets/img/unrender04.jpg" alt="test01" onclick="textureIndex = 3"/>
 </div>
 </div>
 <script src="/assets/js/posts/demo01.js"></script>
--- assets/js/posts/demo01.js
+++ assets/js/posts/demo01.js
@@ -12,33 +12,16 @@ let model = {
 };
 
 let vertexBuffer = null;
-let shaderProgram = null;
-let positionAttributeLocation = null;
-let colorUniformLocation = null;
-function init() {
-	// Vertex Buffer
-	vertexBuffer = gl.createBuffer();
+let testProgram = null; // Shader program for rendering test images
+let evaluationProgram = null; // Shader program for rendering evaluating images
+let testColorUniformLocation = null;
+let testPositionAttributeLocation = null;
+let evaluationPositionAttributeLocation = null;
+let evaluationSamplerUniformLocation = null;
+let textures = [];
+let textureIndex = 0; // Which texture are we training to?
 
-	// Shader Program
-	const vertexSource = `
-		uniform vec3 u_color;
-		attribute vec2 a_position;
-		varying highp vec4 v_color;
-
-		void main() {
-			v_color = vec4(u_color, 1.0);
-			gl_Position = vec4(a_position, 0.0, 1.0);
-		}
-	`;
-
-	const fragSource = `
-		varying highp vec4 v_color;
-
-		void main() {
-			gl_FragColor = v_color;
-		}
-	`;
-
+function loadShaderProgram(vertSrc, fragSrc) {
 	function loadShader(type, source) {
   		const shader = gl.createShader(type);
 
@@ -58,10 +41,10 @@ function init() {
   		return shader;
 	}	
 
-	const vertexShader = loadShader(gl.VERTEX_SHADER, vertexSource);
-	const fragmentShader = loadShader(gl.FRAGMENT_SHADER, fragSource);
+	const vertexShader = loadShader(gl.VERTEX_SHADER, vertSrc);
+	const fragmentShader = loadShader(gl.FRAGMENT_SHADER, fragSrc);
 
-	shaderProgram = gl.createProgram();
+	const shaderProgram = gl.createProgram();
 	gl.attachShader(shaderProgram, vertexShader);
 	gl.attachShader(shaderProgram, fragmentShader);
 	gl.linkProgram(shaderProgram);
@@ -73,10 +56,142 @@ function init() {
 			+ gl.getProgramInfoLog(shaderProgram));
 	}
 	
-	positionAttributeLocation = gl.getAttribLocation(shaderProgram, 'a_position');
-	colorUniformLocation = gl.getUniformLocation(shaderProgram, 'u_color');
+	return shaderProgram;
+}
+
+function init() {
+	// Vertex Buffer
+	vertexBuffer = gl.createBuffer();
+
+	// Shader Programs
+	testProgram = loadShaderProgram(
+		// Vertex Shader
+		`uniform vec3 u_color;
+		attribute vec2 a_position;
+		varying highp vec4 v_color;
+
+		void main() {
+			v_color = vec4(u_color, 1.0);
+			gl_Position = vec4(a_position, 0.0, 1.0);
+		}`,
+		// Fragment Shader
+		`varying highp vec4 v_color;
+
+		void main() {
+			gl_FragColor = v_color;
+		}
+	`);
+
+	evaluationProgram = loadShaderProgram(
+		// Vertex Shader
+		`attribute vec4 a_position;
+		varying highp vec2 v_texture_coord;
+		void main() {
+			v_texture_coord = a_position.zw;
+			gl_Position = vec4(a_position.xy, 0.0, 1.0);
+		}`,
+		// Fragment Shader
+		`varying highp vec2 v_texture_coord;
+		uniform sampler2D u_sampler;
+
+		void main(void) {
+			gl_FragColor = texture2D(u_sampler, v_texture_coord);
+			// gl_FragColor = vec4(0.0, vTextureCoord.x, vTextureCoord.y, 1.0);
+		}`
+	);	
+	testPositionAttributeLocation = gl.getAttribLocation(testProgram, 'a_position');
+	testColorUniformLocation = gl.getUniformLocation(testProgram, 'u_color');
+	evaluationPositionAttributeLocation = gl.getAttribLocation(evaluationProgram, 'a_position');
+	evaluationSamplerUniformLocation = gl.getUniformLocation(evaluationProgram, 'u_sampler');
+
+	function loadTexture(path) {
+		let texture = gl.createTexture();
+
+		// Temp texure while main texture loads
+		gl.bindTexture(gl.TEXTURE_2D, texture);
+		let tempPixel = new Uint8Array([255, 0, 255, 255]); // Magenta
+		gl.texImage2D(
+			gl.TEXTURE_2D,
+			0, // level
+			gl.RGBA, // Internal format
+			1, 1, 0, // width, height, border
+			gl.RGBA, // src format
+			gl.UNSIGNED_BYTE, // src type
+			tempPixel);
+		gl.bindTexture(gl.TEXTURE_2D, null);
+
+		let img = new Image();
+		img.onload = function() {
+			gl.bindTexture(gl.TEXTURE_2D, texture);
+			gl.texImage2D(
+				gl.TEXTURE_2D, 
+				0, // level
+				gl.RGBA, // internal format
+				gl.RGBA, // src format
+				gl.UNSIGNED_BYTE, // src type
+				img);
+
+			gl.generateMipmap(gl.TEXTURE_2D);
+		};
+
+		img.src = path;
+		return texture;
+	}
+
+	textures.push(loadTexture("/assets/img/unrender01.jpg"));
+	textures.push(loadTexture("/assets/img/unrender02.jpg"));
+	textures.push(loadTexture("/assets/img/unrender03.jpg"));
+	textures.push(loadTexture("/assets/img/unrender04.jpg"));
 
 	requestAnimationFrame(draw);
+}
+
+function drawTest() {
+	gl.useProgram(testProgram);
+
+	gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+	let data = new Float32Array(model.vertices);
+	gl.bufferData(gl.ARRAY_BUFFER, data, gl.DYNAMIC_DRAW);
+
+	// Vertex Attrib Pointers
+	gl.vertexAttribPointer(
+		testPositionAttributeLocation,
+		2, // Components
+		gl.FLOAT,
+		false, // Don't normalize
+		0, 0); // No stride, no offset
+	gl.enableVertexAttribArray(testPositionAttributeLocation);
+	gl.uniform3fv(testColorUniformLocation, model.forgroundColor);
+
+	gl.drawArrays(gl.TRIANGLES, 0, 3);
+}
+
+function drawEvaluation() {
+	gl.useProgram(evaluationProgram);
+
+	gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+	// Full screen triangle
+	let data = new Float32Array([
+		-1.0, -1.0, 0.0, 2.0, 
+		-1.0, 3.0, 0.0, 0.0,
+		3.0, -1.0, 2.0, 2.0
+	]);
+	gl.bufferData(gl.ARRAY_BUFFER, data, gl.DYNAMIC_DRAW);
+
+	// Vertex Attrib Pointers
+	gl.vertexAttribPointer(
+		evaluationPositionAttributeLocation,
+		4, // Components
+		gl.FLOAT,
+		false, // Don't normalize
+		0, 0); // stride, offset
+	gl.enableVertexAttribArray(evaluationPositionAttributeLocation);
+
+	gl.activeTexture(gl.TEXTURE0);
+	gl.bindTexture(gl.TEXTURE_2D, textures[textureIndex]);
+	gl.uniform1i(evaluationSamplerUniformLocation, 0); // Tell shader to use texture 0
+
+	gl.drawArrays(gl.TRIANGLES, 0, 3);
 }
 
 function draw () {
@@ -88,23 +203,8 @@ function draw () {
 
 	gl.clear(gl.COLOR_BUFFER_BIT);
 
-	gl.useProgram(shaderProgram);
-
-	gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-	let data = new Float32Array(model.vertices);
-	gl.bufferData(gl.ARRAY_BUFFER, data, gl.DYNAMIC_DRAW);
-
-	// Vertex Attrib Pointers
-	gl.vertexAttribPointer(
-		positionAttributeLocation,
-		2, // Components
-		gl.FLOAT,
-		false, // Don't normalize
-		0, 0); // No stride, no offset
-	gl.enableVertexAttribArray(positionAttributeLocation);
-	gl.uniform3fv(colorUniformLocation, model.forgroundColor);
-
-	gl.drawArrays(gl.TRIANGLES, 0, 3);
+	//drawTest();
+	drawEvaluation();
 
 	requestAnimationFrame(draw);
 }
